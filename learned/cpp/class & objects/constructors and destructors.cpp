8120bd1b0df9b165293856278bd0c21809428343
Mehedi Hasan Maruf <41261534+MjMehediHasan@users.noreply.github.com>
2022-02-11 12:33:26 +0600

    learned constructors with overloads and destructors in class... OOP is ooooonnnn

---
+++ learned/cpp/class & objects/constructors and destructors.cpp
@@ -0,0 +1,51 @@
+// Copyright [2022] Meek_0
+// Learning Started 11/02/22 | 11.00 a.m
+// Constructor and destructors
+
+#include <iostream>
+#include <string>
+
+class player {
+ private:
+    std::string name{"Player"};
+    int health{100};
+    int xp{0};
+ public:
+    void set_name(std::string name) {this->name = name;}    // "this" is a pointer to the object
+    // void set_name(std::string name_input) {name = name_input;}
+
+    // constructors with overloads
+    player() {std::cout << "No argument constructor is called" << std::endl;}
+    player(std::string name) {std::cout << "String constructor created ! for " << name << std::endl;}
+    player(std::string name, int health, int xp) {std::cout << "Three argument constructor called! for " << name << std::endl;}
+
+    // destructor
+    ~player() {std::cout << "Destructor called! for " << name << std::endl;}
+};
+
+int main() {
+    // they are seperated by scopes "{}"
+    // when class is out of scope then destructor is called
+    {
+        player meek;
+        meek.set_name("Meek");
+    }
+    {
+        player Mj;
+        Mj.set_name("Mj");
+        player hero;
+        hero.set_name("Hero");
+        player kim;
+        kim.set_name("Kim");
+        // destructor is called in reverse order of the constructors
+    }
+    {
+    player *enemy = new player;
+    enemy -> set_name("Enemy");
+    delete enemy;
+    player *boss = new player("Boss", 100, 1000);
+    boss -> set_name("Boss");
+    delete boss;
+    }
+    return 0;
+}
