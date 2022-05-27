9f878d587cd320a78b5696c414b2ce81b018e84f
Mehedi Hasan Maruf <41261534+Mehedi-CSE20@users.noreply.github.com>
2022-05-28 02:23:30 +0600

    Create pop_count_and_bitset_for_binarry_to_dec-convertion.cpp

---
+++ Algo/pop_count_and_bitset_for_binarry_to_dec-convertion.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fast   ios_base::sync_with_stdio(0); cin.tie(0);
+
+ll bin(ll &n) {
+    auto bit = std::bitset<32>(n).to_ullong();
+    return __builtin_popcount(bit);
+}
+
+int main() {
+    fast
+    ll x;
+    cin >> x;
+    cout << bin(x) << endl;
+}
