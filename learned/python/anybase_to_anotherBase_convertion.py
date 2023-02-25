06353998562a0e52e26a94cef6ab7fc9a00c7acd
Mehedi Hasan Maruf <41261534+mehedi37@users.noreply.github.com>
2023-02-25 10:19:10 +0600

    Create anybase_to_anotherBase_convertion.py

---
+++ learned/python/anybase_to_anotherBase_convertion.py
@@ -0,0 +1,24 @@
+def divide(a, b, base):
+    # Divide two numbers in any base
+    quotient = ""
+    remainder = 0
+    digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    for digit in str(a):
+        dividend = remainder * base + digits.index(digit)
+        quotient_digit = dividend // b
+        remainder = dividend % b
+        quotient += digits[quotient_digit]
+    return quotient, digits[remainder]
+
+
+if __name__ == "__main__":
+    # print("Enter Number, Base, Expected number base :")
+    base_number, base, convert_to = input().split()
+    ans = ""
+    compare = "0"*len(base_number)
+
+    while (base_number > compare):
+        b, r = divide(base_number, int(convert_to), int(base))
+        ans += r
+        base_number = b
+    print(ans[::-1])    # MSB -> LSB
